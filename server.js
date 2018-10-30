8043765c1678de9c407bad84fc2ba36be30eaa34
Spencer Kuzara <kodaxx@gmail.com>
2018-10-30 11:58:43 -0600

    added function for getting ves rate from another source since bitcoinaverage is inaccurate

--- server.js
+++ server.js
@@ -8,17 +8,42 @@ const cmc = new coinmarketcap()
 const bitcoinAverageUrl = 'https://apiv2.bitcoinaverage.com/indices/global/ticker/short?crypto=BTC'
 const dashBtcUrl = 'https://apiv2.bitcoinaverage.com/indices/crypto/ticker/DASHBTC'
 const poloniexDashUrl = 'https://poloniex.com/public?command=returnTradeHistory&currencyPair=BTC_DASH'
+const cryptoCompareUrl = 'https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=VES'
+
+// get bitcoin's average price against various fiat currencies
+function getVes(url) {
+  let output
+  return new Promise(resolve => {
+    axios.get(url)
+      .then(result => {
+        // for each currency passed into this function, we add a key/value to output (ex. USD: 6500.12345)
+        output = result.data.VES
+        // resolve an object containing all requested currencies
+        resolve(output)
+      })
+      .catch(error => {
+        console.log(`Error: ${error}`)
+        resolve(error)
+      })
+  })
+}
 
 // get bitcoin's average price against various fiat currencies
 function getBitcoinAverage(url, [...currencies]) {
   const output = {}
   return new Promise(resolve => {
     axios.get(url)
-      .then(result => {
+      .then(async result => {
         // for each currency passed into this function, we add a key/value to output (ex. USD: 6500.12345)
         for (var currency of currencies) {
-          // we use the "last" price from bitcoinaverage to give us the most recent exchange rate
-          output[currency] = result.data[`BTC${currency}`].last
+          // we check if the currency is 'VES', since the rates given on BitcoinAverage are incorrect
+          if (currency === 'VES') {
+            // instead we get rates from CryptoCompare
+            output[currency] = await getVes(cryptoCompareUrl)
+          } else {
+            // otherwise we use the "last" price from bitcoinaverage to give us the most recent exchange rate
+            output[currency] = result.data[`BTC${currency}`].last
+          }
         }
         // resolve an object containing all requested currencies
         resolve(output)
@@ -128,16 +153,18 @@ app.get('/*', async function(req, res) {
   let currencies = params.map(currency => currency.toUpperCase())
   // if we want all rates, we simply pass the 'list' parameter
   if (currencies[0] === "LIST") {
-    currencies = ["AED","AFN","ALL","AMD","ANG","AOA","ARS","AUD","AWG","AZN","BAM","BBD","BDT","BGN","BHD","BIF",
-      "BMD","BND","BOB","BRL","BSD","BTN","BWP","BYN","BZD","CAD","CDF","CHF","CLF","CLP","CNH","CNY","COP","CRC",
-      "CUC","CUP","CVE","CZK","DJF","DKK","DOP","DZD","EGP","ERN","ETB","EUR","FJD","FKP","GBP","GEL","GGP","GHS",
-      "GIP","GMD","GNF","GTQ","GYD","HKD","HNL","HRK","HTG","HUF","IDR","ILS","IMP","INR","IQD","IRR","ISK","JEP",
-      "JMD","JOD","JPY","KES","KGS","KHR","KMF","KPW","KRW","KWD","KYD","KZT","LAK","LBP","LKR","LRD","LSL","LYD",
-      "MAD","MDL","MGA","MKD","MMK","MNT","MOP","MRO","MUR","MVR","MWK","MXN","MYR","MZN","NAD","NGN","NIO","NOK",
-      "NPR","NZD","OMR","PAB","PEN","PGK","PHP","PKR","PLN","PYG","QAR","RON","RSD","RUB","RWF","SAR","SBD","SCR",
-      "SDG","SEK","SGD","SHP","SLL","SOS","SRD","SSP","STD","SVC","SYP","SZL","THB","TJS","TMT","TND","TOP","TRY",
-      "TTD","TWD","TZS","UAH","UGX","USD","UYU","UZS","VES","VND","VUV","WST","XAF","XAG","XAU","XCD","XDR","XOF",
-      "XPD","XPF","XPT","YER","ZAR","ZMW","ZWL"]
+    currencies = ["AED", "AFN", "ALL", "AMD", "ANG", "AOA", "ARS", "AUD", "AWG", "AZN", "BAM", "BBD", "BDT", "BGN",
+      "BHD", "BIF", "BMD", "BND", "BOB", "BRL", "BSD", "BTN", "BWP", "BYN", "BZD", "CAD", "CDF", "CHF", "CLF", "CLP",
+      "CNH", "CNY", "COP", "CRC", "CUC", "CUP", "CVE", "CZK", "DJF", "DKK", "DOP", "DZD", "EGP", "ERN", "ETB", "EUR",
+      "FJD", "FKP", "GBP", "GEL", "GGP", "GHS", "GIP", "GMD", "GNF", "GTQ", "GYD", "HKD", "HNL", "HRK", "HTG", "HUF",
+      "IDR", "ILS", "IMP", "INR", "IQD", "IRR", "ISK", "JEP", "JMD", "JOD", "JPY", "KES", "KGS", "KHR", "KMF", "KPW",
+      "KRW", "KWD", "KYD", "KZT", "LAK", "LBP", "LKR", "LRD", "LSL", "LYD", "MAD", "MDL", "MGA", "MKD", "MMK", "MNT",
+      "MOP", "MRO", "MUR", "MVR", "MWK", "MXN", "MYR", "MZN", "NAD", "NGN", "NIO", "NOK", "NPR", "NZD", "OMR", "PAB",
+      "PEN", "PGK", "PHP", "PKR", "PLN", "PYG", "QAR", "RON", "RSD", "RUB", "RWF", "SAR", "SBD", "SCR", "SDG", "SEK",
+      "SGD", "SHP", "SLL", "SOS", "SRD", "SSP", "STD", "SVC", "SYP", "SZL", "THB", "TJS", "TMT", "TND", "TOP", "TRY",
+      "TTD", "TWD", "TZS", "UAH", "UGX", "USD", "UYU", "UZS", "VES", "VND", "VUV", "WST", "XAF", "XAG", "XAU", "XCD",
+      "XDR", "XOF", "XPD", "XPF", "XPT", "YER", "ZAR", "ZMW", "ZWL"
+    ]
   }
   console.log(`get rate: ${currencies}`)
   try {
@@ -149,7 +176,7 @@ app.get('/*', async function(req, res) {
     // multiply each value in the object by the current BTC/DASH rate
     for (var key in rates) {
       if (rates.hasOwnProperty(key)) {
-          rates[key] *= poloniex || dash
+        rates[key] *= poloniex || dash
       }
     }
     // return the rates object
@@ -164,5 +191,5 @@ app.get('/*', async function(req, res) {
 // set server
 const server = app.listen(8081, function() {
   const port = server.address().port;
-  console.log(`DashRates API v0.0.2 running on port ${port}`)
+  console.log(`DashRates API v0.1.2 running on port ${port}`)
 })
